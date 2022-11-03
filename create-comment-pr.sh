17950d37d4b3e7f76376e7af3d89cf5f7db56b43
dhananjay12 <dhananjay1204@gmail.com>
2022-11-04 00:43:44 +0100

    test

--- create-comment-pr.sh
+++ create-comment-pr.sh
@@ -1,20 +1,47 @@
 #!/usr/bin/env bash
 
 PULL_NUMBER=${1}
-echo 'PULL_NUMBER =  $PULL_NUMBER'
+echo "Create comment PULL_NUMBER =  $PULL_NUMBER"
+OWNER="dhananjay12"
+REPO="play-with-github-apis"
+
 source ./checkBannedWords.sh
+source ./get-exsisting-coment.sh $PULL_NUMBER
 
 if [ ${#BANNED_WORDS_FOUND[@]} -eq 0 ]; then
     echo "No errors, hooray"
-else
-  COMMENT_BODY="{\"body\":\"You have these banned words in the file : $BANNED_WORDS_FOUND\", \"event\":\"REQUEST_CHANGES\" }"
-  echo "$COMMENT_BODY"
+    if [[ -n $REVIEW_ID ]]; then
+    echo "https://api.github.com/repos/$OWNER/$REPO/pulls/$PULL_NUMBER/reviews/$REVIEW_ID"
     curl \
-      -X POST \
+      -X DELETE \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-      https://api.github.com/repos/dhananjay12/play-with-github-apis/pulls/$PULL_NUMBER/reviews \
-      -d "$COMMENT_BODY"
+      https://api.github.com/repos/$OWNER/$REPO/pulls/$PULL_NUMBER/reviews/$REVIEW_ID
+   fi
+else
+  if [[ -n $REVIEW_ID ]];
+  then
+        echo "updating Existing review"
+        COMMENT_BODY="{\"body\":\"You have these banned words in the file : $BANNED_WORDS_FOUND\" }"
+        echo $COMMENT_BODY
+        curl \
+          -X PUT \
+          -H "Accept: application/vnd.github+json" \
+          -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+          https://api.github.com/repos/$OWNER/$REPO/pulls/$PULL_NUMBER/reviews/$REVIEW_ID \
+          -d "$COMMENT_BODY"
+  else
+        echo "updating Existing review"
+        COMMENT_BODY="{\"body\":\"You have these banned words in the file : $BANNED_WORDS_FOUND\", \"event\":\"REQUEST_CHANGES\" }"
+        echo $COMMENT_BODY
+        curl \
+          -X POST \
+          -H "Accept: application/vnd.github+json" \
+          -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+          https://api.github.com/repos/$OWNER/$REPO/pulls/$PULL_NUMBER/reviews \
+          -d "$COMMENT_BODY"
+  fi
+
 
 fi
 
