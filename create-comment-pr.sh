99a82957e8317f4d16cc687f1711d7e16ec34930
dhananjay12 <dhananjay1204@gmail.com>
2022-11-03 14:11:53 +0100

    test

--- create-comment-pr.sh
+++ create-comment-pr.sh
@@ -7,14 +7,14 @@ source ./checkBannedWords.sh
 if [ ${#BANNED_WORDS_FOUND[@]} -eq 0 ]; then
     echo "No errors, hooray"
 else
-  COMMENT_BODY='{"body":"You have these banned words in the file : $BANNED_WORDS_FOUND", "event":"REQUEST_CHANGES" }'
+  COMMENT_BODY="{\"body\":\"You have these banned words in the file : $BANNED_WORDS_FOUND\", \"event\":\"REQUEST_CHANGES\" }"
   echo "$COMMENT_BODY"
     curl \
       -X POST \
       -H "Accept: application/vnd.github+json" \
       -H "Authorization: Bearer ${GITHUB_TOKEN}" \
       https://api.github.com/repos/dhananjay12/play-with-github-apis/pulls/$PULL_NUMBER/reviews \
-      -d $COMMENT_BODY
+      -d "$COMMENT_BODY"
 
 fi
 
